85e8ab65c5022d17552c16476a7e1b822a8defd4
aldelsa <aldelsa1@inf.upv.es>
2018-09-26 00:05:44 +0200

    until 2.14

--- Exercises/exercises01.scala
+++ Exercises/exercises01.scala
@@ -13,15 +13,35 @@ object exercises01{
     //2.4 Sum of digits
     //println(sumDigits(566))
 
-    //2.5 Greatest Common Divisor ####### NOT
+    //2.5 Greatest Common Divisor ####### NOT ######
     //println(gcd(10,55))
 
     //2.6 Fibonacci
     //println(fibonacci(7))
 
     //2.7 Pascal triangle
-    println(pascal(3,2))
+    //println(pascal(3,2))
 
+    //2.8 Currified function  ####### NOT ######
+    //println(currified(3,2))
+
+    //2.9 Sum elements
+    //println(sumEle(List(1,2,3,4,5)))
+
+    //2.10 Square elements
+    //println(square(List(1,2,3,4,5)))
+
+    //2.11 Largest element
+    //println(largest(List(1,2,7,4,5)))
+
+    //2.12 Reverse  ####### NOT ######
+    //println(reverse(List(1,2,7,4,5)))
+
+    //2.13 Map
+    //println(map(List(1, 2, 3, 4, 5), x => x * x))
+
+    //2.14 Sorted
+    println(sorted(List(5, 4, 3, 2, 1), (a, b) => a > b))
   }
 
   ///////////////////////////////////////////
@@ -87,4 +107,69 @@ object exercises01{
     if (y == 1 || x == y) 1
     else pascal( x-1, y-1 ) + pascal ( x-1, y)
   }
+
+  ///////////////////////////////////////////
+  //2.8 Currified function
+  ///////////////////////////////////////////
+  def currified(num1: Integer , num2: Integer) = {
+
+  }
+
+  ///////////////////////////////////////////
+  //2.9 Sum elements
+  ///////////////////////////////////////////
+  def sumEle(l: List[Int]): Int = {
+    if (l.isEmpty) 0
+    else l.head + sumEle(l.tail)
+  }
+
+  ///////////////////////////////////////////
+  //2.10 Square elements
+  ///////////////////////////////////////////
+  def square(l: List[Int]): List[Int] = {
+    if (l.isEmpty) Nil
+    else l.head * l.head :: square(l.tail)
+  }
+
+  ///////////////////////////////////////////
+  //2.11 Largest element
+  ///////////////////////////////////////////
+  def largest(l: List[Int]): Int = {
+    def largest_1(l: List[Int], max: Int): Int = {
+      if (l.isEmpty) max
+      else if (l.head > max) largest_1(l.tail,l.head)
+      else largest_1(l.tail,max)
+    }
+    largest_1(l,0)
+  }
+
+  ///////////////////////////////////////////
+  //2.12 Reverse
+  ///////////////////////////////////////////
+  //def reverse(l: List[Int]): List[Int] = {
+    //if (l.isEmpty) Nil
+    //else (reverse(l.tail)) l.head
+  //}
+
+  ///////////////////////////////////////////
+  //2.13 Map
+  ///////////////////////////////////////////
+  def map(l: List[Int], f: Int => Int): List[Int] = {
+    if (l.isEmpty) Nil
+    else f(l.head) :: map(l.tail,f)
+  }
+
+  ///////////////////////////////////////////
+  //2.14 Sorted
+  ///////////////////////////////////////////
+  def sorted (l: List[Int], f: (Int,Int) => Boolean): Boolean = {
+    def sorted_1 (l: List[Int], f: (Int,Int) => Boolean, h: Integer): Boolean = {
+      if (l.isEmpty) true
+      else if (f(h,l.head)) sorted_1(l.tail,f,l.head)
+      else false
+    }
+    sorted_1(l.tail,f,l.head)
+  }
+
+
 }
