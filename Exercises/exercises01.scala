143269f3b4ca2ba18fa52727cb740d2e7639d926
aldelsa <aldelsa1@inf.upv.es>
2018-09-25 23:09:31 +0200

    Until 2.7

--- Exercises/exercises01.scala
+++ Exercises/exercises01.scala
@@ -1,16 +1,31 @@
 object exercises01{
   def main(args: Array[String]):Unit = {
 
+    //2.1 Sum
     //println(sum(10))
+
+    //2.2 Factorial
     //println(factorial(5))
+
+    //2.3 Digits
     //println(digits(5634))
+
+    //2.4 Sum of digits
     //println(sumDigits(566))
+
+    //2.5 Greatest Common Divisor ####### NOT
     //println(gcd(10,55))
+
+    //2.6 Fibonacci
     //println(fibonacci(7))
+
+    //2.7 Pascal triangle
     println(pascal(3,2))
 
   }
 
+  ///////////////////////////////////////////
+  //2.1 Sum
   ///////////////////////////////////////////
   def sum(num: Integer) : Integer = {
     if (num <= 0 ) 0
@@ -18,11 +33,15 @@ object exercises01{
   }
 
   ///////////////////////////////////////////
+  //2.2 Factorial
+  ///////////////////////////////////////////
   def factorial(num: Integer) : Integer = {
     if (num <= 1 ) 1
     else num * factorial(num - 1)
   }
 
+  ///////////////////////////////////////////
+  //2.3 Digits
   ///////////////////////////////////////////
   def digits(num: Integer) : Integer = {
     def digits_2(num: Integer, accu: Integer) : Integer = {
@@ -33,6 +52,8 @@ object exercises01{
   }
 
   ///////////////////////////////////////////
+  //2.4 Sum of digits
+  ///////////////////////////////////////////
   def sumDigits(num: Integer ) : Integer = {
     def sumDigits_2(num: Integer, accu: Integer) : Integer = {
       if (num <= 0 ) accu
@@ -41,12 +62,16 @@ object exercises01{
     sumDigits_2(num,0)
   }
 
+  ///////////////////////////////////////////
+  //2.5 Greatest Common Divisor
+  ///////////////////////////////////////////
   def gcd(num1: Integer , num2: Integer) = {
 
   }
 
   ///////////////////////////////////////////
-
+  //2.6 Fibonacci
+  ///////////////////////////////////////////
   def fibonacci(pos: Int) = {
     def fibonacci_2(n: Int, curr: Int, next: Int): Int = {
       if (n == 0) curr
@@ -55,6 +80,8 @@ object exercises01{
     fibonacci_2(pos, 0, 1)
   }
 
+  ///////////////////////////////////////////
+  //2.7 Pascal triangle
   ///////////////////////////////////////////
   def pascal (x: Int, y: Int): Int = {
     if (y == 1 || x == y) 1
